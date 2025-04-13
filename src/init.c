5b073d18b79b9b8364e9df92b978b6deceff98fe
ownleex <145456737+ownleex@users.noreply.github.com>
2025-04-14 00:19:07 +0200

    feat: Améliorer l'initialisation du jeu et des textures

--- src/init.c
+++ src/init.c
@@ -6,70 +6,103 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 23:31:04 by ayarmaya          #+#    #+#             */
-/*   Updated: 2025/04/13 23:43:33 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2025/04/14 00:18:14 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-bool	create_texture(t_game *game, t_texture *texture, int width, int height)
-{
-	texture->img = mlx_new_image(game->mlx, width, height);
-	if (!texture->img)
-		return (false);
-	texture->addr = mlx_get_data_addr(texture->img, &texture->bits_per_pixel,
-			&texture->line_length, &texture->endian);
-	texture->width = width;
-	texture->height = height;
-	return (true);
-}
-
-bool	init_mlx(t_game *game)
-{
-	game->mlx = mlx_init();
-	if (!game->mlx)
-		return (false);
-	game->win = mlx_new_window(game->mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "cub3D");
-	if (!game->win)
-		return (false);
-	game->img = mlx_new_image(game->mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
-	if (!game->img)
-		return (false);
-	game->img_addr = mlx_get_data_addr(game->img, &game->img_bits_per_pixel,
-			&game->img_line_length, &game->img_endian);
-	return (true);
-}
-
-bool	init_texture_data(t_game *game)
-{
-	bool	success;
-
-	success = true;
-	if (!parse_textures(game))
-		success = false;
-	game->textures_loaded = success;
-	return (success);
-}
-
-bool	init_player(t_game *game)
-{
-	// Player direction and plane are initialized during map parsing
-	// based on the player's starting orientation (N, S, E, W)
-	// This function is called to ensure the player has been initialized correctly
-	if (game->player.dir.x == 0 && game->player.dir.y == 0)
-		return (false);
-	if (game->player.plane.x == 0 && game->player.plane.y == 0)
-		return (false);
-	return (true);
-}
-
+/**
+ * Initialise la structure du jeu
+ * @param game Pointeur vers la structure du jeu
+ * @return true si l'initialisation réussit, false sinon
+ */
 bool	init_game(t_game *game)
 {
-	if (!init_mlx(game))
-		return (false);
-	if (!init_texture_data(game))
-		return (false);
-	if (!init_player(game))
-		return (false);
-	return (true);
+    // Initialiser tous les champs à 0 ou NULL
+    ft_bzero(game, sizeof(t_game));
+    
+    // Initialiser le moteur MLX
+    if (!init_mlx(game))
+        return (false);
+    
+    // Définir que les textures ne sont pas chargées
+    game->textures_loaded = false;
+    
+    return (true);
+}
+
+/**
+ * Initialise la bibliothèque MLX
+ * @param game Pointeur vers la structure du jeu
+ * @return true si l'initialisation réussit, false sinon
+ */
+bool	init_mlx(t_game *game)
+{
+    // Initialiser la connexion MLX
+    game->mlx = mlx_init();
+    if (!game->mlx)
+    {
+        ft_printf("Error\nFailed to initialize MLX\n");
+        return (false);
+    }
+    
+    // Créer la fenêtre
+    game->win = mlx_new_window(game->mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "cub3D");
+    if (!game->win)
+    {
+        ft_printf("Error\nFailed to create window\n");
+        return (false);
+    }
+    
+    // Créer une image pour le rendu
+    game->img.img = mlx_new_image(game->mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
+    if (!game->img.img)
+    {
+        ft_printf("Error\nFailed to create image\n");
+        return (false);
+    }
+    
+    // Obtenir les informations de l'image
+    game->img.addr = mlx_get_data_addr(game->img.img, &game->img.bits_per_pixel,
+            &game->img.line_length, &game->img.endian);
+    
+    // Initialiser la taille de l'image
+    game->img.width = WINDOW_WIDTH;
+    game->img.height = WINDOW_HEIGHT;
+    
+    return (true);
+}
+
+/**
+ * Initialise les textures du jeu
+ * @param game Pointeur vers la structure du jeu
+ * @return true si l'initialisation réussit, false sinon
+ */
+bool	init_texture(t_game *game)
+{
+    // Vérifier que MLX est initialisé
+    if (!game->mlx)
+    {
+        ft_printf("Error\nMLX not initialized before loading textures\n");
+        return (false);
+    }
+    
+    // Si les textures ont déjà été chargées, ne pas les recharger
+    if (game->textures_loaded)
+        return (true);
+    
+    // Créer une image provisoire pour vérifier que MLX fonctionne correctement
+    void *test_img = mlx_new_image(game->mlx, 1, 1);
+    if (!test_img)
+    {
+        ft_printf("Error\nFailed to create test image\n");
+        return (false);
+    }
+    mlx_destroy_image(game->mlx, test_img);
+    
+    // Marquer les textures comme chargées
+    game->textures_loaded = true;
+    
+    return (true);
 }
--- src/main.c
+++ src/main.c
@@ -6,55 +6,70 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 23:29:49 by ayarmaya          #+#    #+#             */
-/*   Updated: 2025/04/13 23:30:49 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2025/04/14 00:18:47 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cub3d.h"
+#include "../include/cub3d.h"
 
-int	key_press(int keycode, t_game *game)
-{
-	if (keycode == KEY_ESC)
-		clean_exit(game, EXIT_SUCCESS);
-	else if (keycode == KEY_W)
-		move_forward(game);
-	else if (keycode == KEY_S)
-		move_backward(game);
-	else if (keycode == KEY_A)
-		move_left(game);
-	else if (keycode == KEY_D)
-		move_right(game);
-	else if (keycode == KEY_LEFT)
-		rotate(game, -ROT_SPEED);
-	else if (keycode == KEY_RIGHT)
-		rotate(game, ROT_SPEED);
-	return (0);
-}
-
+/**
+ * Point d'entrée du programme
+ * @param argc Nombre d'arguments
+ * @param argv Tableau des arguments
+ * @return 0 si le programme s'exécute correctement, 1 sinon
+ */
 int	main(int argc, char **argv)
 {
-	t_game	game;
+    t_game  game;
 
-	ft_memset(&game, 0, sizeof(t_game));
-	if (argc != 2)
-	{
-		ft_printf("Error\nUsage: ./cub3D <map.cub>\n");
-		return (EXIT_FAILURE);
-	}
-	if (!is_valid_ext(argv[1], ".cub"))
-	{
-		ft_printf("Error\nInvalid file extension. Expected .cub\n");
-		return (EXIT_FAILURE);
-	}
-	if (!parse_file(&game, argv[1]))
-		return (EXIT_FAILURE);
-	if (!init_game(&game))
-		clean_exit(&game, EXIT_FAILURE);
-	
-	mlx_hook(game.win, 2, 1L << 0, key_press, &game);
-	mlx_hook(game.win, 17, 0, exit_hook, &game);
-	mlx_loop_hook(game.mlx, render, &game);
-	mlx_loop(game.mlx);
-	
-	return (EXIT_SUCCESS);
+    // Vérifier les arguments
+    if (argc != 2)
+    {
+        ft_printf("Error\nUsage: %s <map.cub>\n", argv[0]);
+        return (1);
+    }
+
+    // Initialiser la structure du jeu
+    if (!init_game(&game))
+    {
+        ft_printf("Error\nFailed to initialize game\n");
+        return (1);
+    }
+
+    // Analyser le fichier de carte
+    if (!parse_file(&game, argv[1]))
+    {
+        clean_exit(&game, 1);
+    }
+
+    // Configurer les hooks pour la gestion des événements
+    mlx_hook(game.win, 2, 1L << 0, key_press, &game);         // Touche enfoncée
+    mlx_hook(game.win, 17, 0, exit_hook, &game);             // Croix de fermeture
+    
+    // Lancer la boucle principale
+    mlx_loop_hook(game.mlx, render, &game);
+    mlx_loop(game.mlx);
+
+    return (0);
+}
+
+/**
+ * Fonction de hook pour la boucle principale
+ * Cette fonction est appelée à chaque frame
+ * @param game Pointeur vers la structure du jeu
+ * @return 0 toujours
+ */
+int	render(t_game *game)
+{
+    // Vérifier que les structures nécessaires sont initialisées
+    if (!game || !game->mlx || !game->win || !game->img.img)
+        return (0);
+
+    // Effectuer le raycasting pour calculer la scène 3D
+    raycasting(game);
+    
+    // Afficher l'image à l'écran
+    mlx_put_image_to_window(game->mlx, game->win, game->img.img, 0, 0);
+    
+    return (0);
 }
